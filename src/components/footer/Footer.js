c52230bf13dc4a8e9999ae346eb5f55974a4ef17
lightpc90 <afoageektech@gmail.com>
2024-01-24 13:13:31 +0100

    added bg to the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-      <div className='flex flex-col w-full h-[150px] bg-white mt-10'>
-          <div>CTA</div>
-          <div>Site map</div>
-          <p></p>
+    <div className="flex flex-col w-full min-h-[300px] p-2 text-white bg-gradient-to-br from-[#6d28d9] via-[#6b21a8] to-[#581c87] mt-10">
+      <div>CTA</div>
+      <div>Site map</div>
+      <p></p>
     </div>
-  )
+  );
 }
 
 export default Footer
