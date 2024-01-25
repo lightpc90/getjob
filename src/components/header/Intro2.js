3cd010191fdff92c11a760d56ba40cf078276be5
lightpc90 <afoageektech@gmail.com>
2024-01-25 14:14:35 +0100

    to lowercase

--- src/components/header/Intro2.js
+++ src/components/header/Intro2.js
@@ -7,7 +7,7 @@ const Intro2 = () => {
   return (
     <div className=" flex flex-col justify-between text-xl h-[230px] bg-[#3b0764] w-full pt-[50px] text-white p-3">
       <div className="flex gap-2 ">
-        <p>...Explore endless possibilities</p>
+        <p>...explore endless possibilities</p>
         <SiAzuredataexplorer />
       </div>
       <Marquee><FeatureJobs/></Marquee>
