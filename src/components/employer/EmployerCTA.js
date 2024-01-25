84c02565f966f1691f9a84a2565fab4becf6dbca
lightpc90 <afoageektech@gmail.com>
2024-01-25 21:13:48 +0100

    added image employer component

--- src/components/employer/EmployerCTA.js
+++ src/components/employer/EmployerCTA.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Image from "next/image";
+import employer from "../../../public/Saly-10.svg";
 
 const EmployerCTA = () => {
   return (
-    <div className="flex flex-col gap-3 border-2 border-[#3b0764] text-[#3b0764] rounded-lg px-5 py-10 m-5 shadow-md">
+    <div className="flex flex-col gap-3 border-2 border-[#3b0764] text-[#3b0764] rounded-lg px-5 pb-10 m-5 shadow-md">
+      <Image src={employer} alt="employer image" width={900} height={900} className="justify-center items-center mb-3"/>
       <p className="text-xl">FOR EMPLOYERS</p>
-      <p className="text-2xl font-bold">
-        Searching for the right talent?
-      </p>
+      <p className="text-2xl font-bold">Searching for the right talent?</p>
       <p className="font-semibold">
         Create a free account, complete your profile, list your job vacancies.
         See the list of talents, fit for your organisation!
